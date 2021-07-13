a8db4828671c7d4d7ca3254dbee60abc8c4f4280
Manuel Restrepo <manolo.1217@hotmail.com>
2021-07-13 14:34:40 -0500

    add button

--- script/logica.js
+++ script/logica.js
@@ -1,6 +1,101 @@
 let mostrar = document.getElementById("mostrar");
+
 let bntAmazonas = document.getElementById("bntAmazonas");
 let leticia = document.getElementById("leticia");
+
+let bntAntioquia = document.getElementById("bntAntioquia");
+let medellin = document.getElementById("medellin");
+
+let bntArauca = document.getElementById("bntArauca");
+let arauca = document.getElementById("arauca");
+
+let bntAtlantico = document.getElementById("bntAtlantico");
+let barraquilla = document.getElementById("barraquilla");
+
+let bntBolivar = document.getElementById("bntBolivar");
+let cartagena = document.getElementById("cartagena");
+
+let bntBoyaca = document.getElementById("bntBoyaca");
+let tunja = document.getElementById("tunja");
+
+let bntCaldas = document.getElementById("bntCaldas");
+let manizales = document.getElementById("manizales");
+
+let bntCaqueta = document.getElementById("bntCaqueta");
+let florencia = document.getElementById("florencia");
+
+let bntCasanare = document.getElementById("bntCasanare");
+let yopal = document.getElementById("yopal");
+
+let bntCauca = document.getElementById("bntCauca");
+let popayan = document.getElementById("popayan");
+
+let bntCesar = document.getElementById("bntCesar");
+let valledupar = document.getElementById("valledupar");
+
+let bntChoco = document.getElementById("bntChoco");
+let quibdo = document.getElementById("quibdo");
+
+let bntCordoba = document.getElementById("bntCordoba");
+let monteria = document.getElementById("monteria");
+
+let bntCundinamarca = document.getElementById("bntCundinamarca");
+let bogota = document.getElementById("bogota");
+
+let bntGuania = document.getElementById("bntGuania");
+let inirida = document.getElementById("inirida");
+
+let bntGuaviare = document.getElementById("bntGuaviare");
+let sanJose = document.getElementById("sanJose");
+
+let bntHuila = document.getElementById("bntHuila");
+let neiva = document.getElementById("neiva");
+
+let bntGuajira = document.getElementById("bntGuajira");
+let riohacha = document.getElementById("riohacha");
+
+let bntMagdalena = document.getElementById("bntMagdalena");
+let santaMarta = document.getElementById("santaMarta");
+
+let bntMeta = document.getElementById("bntMeta");
+let villavicencio = document.getElementById("villavicencio");
+
+let bntNariño = document.getElementById("bntNariño");
+let pasto = document.getElementById("pasto");
+
+let bntNSantander = document.getElementById("bntNSantander");
+let cucuta = document.getElementById("cucuta");
+
+let bntPutumayo = document.getElementById("bntPutumayo");
+let mocoa = document.getElementById("mocoa");
+
+let bntQuindio = document.getElementById("bntQuindio");
+let armenia = document.getElementById("armenia");
+
+let bntRisaralda = document.getElementById("bntRisaralda");
+let pereira = document.getElementById("pereira");
+
+let bntSanAndres = document.getElementById("bntSanAndres");
+let sanAndres = document.getElementById("sanAndres");
+
+let bntSantander = document.getElementById("bntSantander");
+let bucaramanga = document.getElementById("bucaramanga");
+
+let bntSucre = document.getElementById("bntSucre");
+let sincelejo = document.getElementById("sincelejo");
+
+let bntTolima = document.getElementById("bntTolima");
+let ibague = document.getElementById("ibague");
+
+let bntValle = document.getElementById("bntValle");
+let cali = document.getElementById("cali");
+
+let bntVaupes = document.getElementById("bntVaupes");
+let mitu = document.getElementById("mitu");
+
+let bntVichada = document.getElementById("bntVichada");
+let puerto = document.getElementById("puerto");
+
 
 
 
@@ -9,7 +104,6 @@ let leticia = document.getElementById("leticia");
 const fnAmazonas = ()=>{
    window.open(leticia);
 }
-
 //eventos
 bntAmazonas.onclick = function () {
     fnAmazonas();

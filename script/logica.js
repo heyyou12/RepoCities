d07c37e7f096c5f895986a152b655e8b2625a3d8
Manuel Restrepo <manolo.1217@hotmail.com>
2021-07-13 14:01:37 -0500

    first commit

---
+++ script/logica.js
@@ -0,0 +1,16 @@
+let mostrar = document.getElementById("mostrar");
+let bntAmazonas = document.getElementById("bntAmazonas");
+let leticia = document.getElementById("leticia");
+
+
+
+
+//funciones
+const fnAmazonas = ()=>{
+   window.open(leticia);
+}
+
+//eventos
+bntAmazonas.onclick = function () {
+    fnAmazonas();
+}
